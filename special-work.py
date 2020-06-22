8348ae21bb20786fc3d26d14e00afd2fbbde02d4
T A H A <mr.ttahaaa@gmail.com>
2020-06-22 21:03:37 +0300

    Special Information About Methods

--- special-work.py
+++ special-work.py
@@ -1,3 +1,4 @@
+"""
 name = "'Sandrine'"
 surName = "'Wilderman'"
 city = "'Mantemouth'"
@@ -5,5 +6,9 @@ phoneNumber = "'851.494.1217'"
 companyName = "'Inc,and Sons,LLC,Group'"
 finansNumber = "'85196534'"
 
-combination = "My name is {} and my surname is {}. I live in {}. My company name is {}.Finally my finans number is {}. ".format(name,surName,city,companyName,finansNumber)
-print(combination)
+result = "My name is {} and my surname is {}. I live in {}. My company name is {}.Finally my finans number is {}. ".format(name,surName,city,companyName,finansNumber)
+
+print(result)
+"""
+
+
