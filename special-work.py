3047cb69fc5fee6c7f803945009e771585ceddd8
T A H A <mr.ttahaaa@gmail.com>
2020-06-22 20:57:16 +0300

    Special Information About Methods

---
+++ special-work.py
@@ -0,0 +1,9 @@
+name = "'Sandrine'"
+surName = "'Wilderman'"
+city = "'Mantemouth'"
+phoneNumber = "'851.494.1217'"
+companyName = "'Inc,and Sons,LLC,Group'"
+finansNumber = "'85196534'"
+
+combination = "My name is {} and my surname is {}. I live in {}. My company name is {}.Finally my finans number is {}. ".format(name,surName,city,companyName,finansNumber)
+print(combination)
