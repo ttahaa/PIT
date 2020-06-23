16b6c1b146feef0abd90f29ac05bdbf6a76b266d
T A H A <mr.ttahaaa@gmail.com>
2020-06-23 12:10:31 +0300

    String Methods

--- special-work.py
+++ special-work.py
@@ -10,18 +10,30 @@ result = "My name is {} and my surname is {}. I live in {}. My company name is {
 
 print(result)
 """
-explanation = "Hello world.We are alive.Come in there is no thunder here"
+explanation = "Hello world.We are alive.Come in there is no thunder here."
 
 result = explanation.capitalize()
-# Hello world.we are alive.come in there is no thunder here
+# This method just makes capitalizes the first letter.
 
 result = explanation.casefold()
-# hello world.we are alive.come in there is no thunder here
+# This method makes all letters lowercase.
 
-result = explanation.center(100,"*")
-#*********************Hello world.We are alive.Come in there is no thunder here**********************
+result = explanation.center(100,"/")
+# This method puts the symbol at the end and beginning of the string.
 
-result = explanation.count("e")
-#10 Ama  ("e",15) deseydik 15. karakterden sonraki "e" leri sayardı.
+result = explanation.count(" ")
+# This method indicates the number of times the symbol is written.
+
+result = explanation.encode("utf-8")
+# This method specifies an encoding style.
+
+result = explanation.endswith("e")
+# This method checks which character it ends with.
+
+result = explanation.expandtabs(50)
+# Put \ t wherever you want.Then write the number you want.Finally there will be as many spaces as you write.
+
+result = explanation.find("w")
+# This method checks the presence of the typed symbol.And prints what order it are in.
 
 print(result)
