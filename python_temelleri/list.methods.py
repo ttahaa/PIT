ed7bd5d9d48b2ac8c45bda8b5e315e82e7c68bbd
T A H A <mr.ttahaaa@gmail.com>
2020-06-23 16:52:31 +0300

    Python Training Files

---
+++ python_temelleri/list.methods.py
@@ -0,0 +1,19 @@
+numbers  = [1, 10, 5, 16, 4, 9, 10]
+letters = ["a", "g", "s", "b", "y","a","s"]
+
+val = min(numbers)
+val = max(numbers)
+val = min(letters)
+val = max(letters)
+
+val = numbers[3:6]
+val = numbers[:3]
+val = numbers[4:]
+
+numbers[4] = 40
+
+numbers.append(49)
+
+
+
+print(numbers)
---
+++ python_temelleri/strings.py
@@ -0,0 +1,16 @@
+name = "Taha"
+surname = "Özdere"
+age = 14
+
+greeting = "My name is "+ name + " "+ surname + " and \nI am "+ str(age) +" years old"
+length = len(greeting)
+
+# print(greeting)
+# print(greeting[0])
+# print(greeting[3])
+# print(greeting[length-1])
+# print(greeting[-1])
+# print(greeting[3:7])
+# print(greeting[3:])
+# print(greeting[:15])
+print(greeting[2:40:3])
